d31de57df6e536270db803ec1e12847f006b067c
angong_tong <angongtong@outlook.com>
2023-04-21 14:48:07 +0930

    grid established

---
+++ tic_tac_toe.js
@@ -0,0 +1,6 @@
+var article = document.querySelector("box-container");
+var player1 = document.querySelector()
+var player2 = document.querySelector()
+body.addEventListener("click", function(event) {
+    var article = event.target
+})
