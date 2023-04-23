84774b4bff2982d8341ad5d19b316cf14a34c3b1
angong_tong <angongtong@outlook.com>
2023-04-23 22:53:32 +0930

    assigning the xo characters to player one and two

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,6 +1,23 @@
-var div = document.querySelector("box-container");
-var player1 = document.querySelector("player1")
-var player2 = document.querySelector("player2")
-body.addEventListener("click", function(event) {
-    var div = event.target
-})
+// var div = document.querySelector("box-container");
+// var player1 = document.querySelector("player1")
+// var player2 = document.querySelector("player2")
+// body.addEventListener("click", function(event) {
+//     var div = event.target
+// })
+
+var body = document.querySelector("body");
+var div = document.querySelector("box-item");
+
+body.addEventListener("click", function(click) {
+    var click = event.target
+    var player1 = event.target
+    if (click .classList.contains("box-item")) {
+        event.target.textContent = "X"
+    }
+    console.log(click)
+})    
+
+// I am now going to code for the Tic Tac Toe game by first assigning player 1 to "X" and player 2 to "O"
+
+// Then I will make a table of winning combinations that will help conclude the game as a loss or win for either player.
+// As well as an else statement for a tie when neither player wins.
